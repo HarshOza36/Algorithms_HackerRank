d441f459e965441c7e54b2fb479b7cb1a6dc12ea
HarshOza36 <harshoza3636@gmail.com>
2021-08-01 10:54:36 +0530

    quick and counting sort

---
+++ Sorting/quickSort1_partition.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'quickSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def quickSort(arr):
+    # Write your code here
+    pivot = arr[0]
+    l, r = [], []
+    for i in arr:
+        if(i == pivot):
+            continue
+        elif(i < pivot):
+            l.append(i)
+        elif(i > pivot):
+            r.append(i)
+    return l + [pivot] + r
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = quickSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
